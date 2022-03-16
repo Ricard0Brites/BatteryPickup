bc8b78d48bdb9d78ecde12c6994e18bcb438b2b9
Ricardo Brites <ricardo.brites.work@gmail.com>
2022-03-16 00:42:39 +0000

    Setup
    Created a base class for consumable items called "Pickup"
        State: active/Inactive
        Functions: Getter, Setter
    
    Created a BP for the Pickup Class, understanding proposes only (Can be deleted later on)

---
+++ Source/BatteryPickup/Battery_Pickup.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Pickup.h"
+#include "Battery_Pickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTERYPICKUP_API ABattery_Pickup : public APickup
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/BatteryPickup/Pickup.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Pickup.generated.h"
+
+UCLASS()
+class BATTERYPICKUP_API APickup : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APickup();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	/* Return the mesh for the pickup */
+	FORCEINLINE UStaticMeshComponent* GetMesh() const { return pickupMesh; }
+
+protected:
+	bool bIsActive;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pickup", meta = (allowprivateaccess = true))
+	UStaticMeshComponent* pickupMesh;
+
+	// function to return the variable value
+	UFUNCTION(BlueprintPure, category = "Pickup")
+	bool IsActive();
+
+	//function to change the value of the variable
+	UFUNCTION(BlueprintCallable, Category = "Pickup")
+	void SetActive(bool NewPickupState);
+};
