c68583fe1737153d7090ebbc3839c52c256b1592
Ricardo Brites <ricardo.brites.work@gmail.com>
2022-03-29 10:58:36 +0100

    Multiple
    added hud (Bugged)
    added variables and functions to track power points (Possibly bugged)

--- Source/BatteryPickup/BatteryPickupGameMode.cpp
+++ Source/BatteryPickup/BatteryPickupGameMode.cpp
@@ -3,6 +3,8 @@
 #include "BatteryPickupGameMode.h"
 #include "BatteryPickupCharacter.h"
 #include "BatteryPickup/BatteryPickupCharacter.h"
+#include "BatteryPickup.h"
+#include "Blueprint/UserWidget.h"
 #include <Kismet/GameplayStatics.h>
 #include "UObject/ConstructorHelpers.h"
 
@@ -17,6 +19,31 @@ ABatteryPickupGameMode::ABatteryPickupGameMode()
 	}
 	// base decay rate
 	DecayRate = 0.01f;
+}
+
+void ABatteryPickupGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	ABatteryPickupCharacter* MyCharacter = Cast<ABatteryPickupCharacter>(UGameplayStatics::GetPlayerPawn(this, 0));
+	SetCurrentState(EBatteryPlayState::EPlaying);
+	if (MyCharacter)
+	{
+		// If our power is greater than needed to win, set the game's state to Won
+		if (MyCharacter->GetCurrentPower() > PowerToWin)
+		{
+			SetCurrentState(EBatteryPlayState::EWon);
+		}
+		// if the character's power is positive
+		else if (MyCharacter->GetCurrentPower() > 0)
+		{
+			// decrease the character's power using the decay rate
+			MyCharacter->UpdatePower(DecayRate * (MyCharacter->GetInitialPower()));
+		}
+		else
+		{
+			SetCurrentState(EBatteryPlayState::EGameOver);
+		}
+	}
 }
 
 void ABatteryPickupGameMode::Tick(float DeltaTime)
@@ -34,4 +61,19 @@ void ABatteryPickupGameMode::Tick(float DeltaTime)
 			MyCharacter->UpdatePower(-DeltaTime * DecayRate * (MyCharacter->GetInitialPower()));
 		}
 	}
+}
+
+float ABatteryPickupGameMode::GetPowerToWin()
+{
+	return PowerToWin;
+}
+
+EBatteryPlayState ABatteryPickupGameMode::GetCurrentState() const
+{
+	return CurrentState;
+}
+
+void ABatteryPickupGameMode::SetCurrentState(EBatteryPlayState state)
+{
+	CurrentState = state;
 }
--- Source/BatteryPickup/BatteryPickupGameMode.h
+++ Source/BatteryPickup/BatteryPickupGameMode.h
@@ -6,6 +6,16 @@
 #include "GameFramework/GameModeBase.h"
 #include "BatteryPickupGameMode.generated.h"
 
+// enum to store the current state of game play
+UENUM(BlueprintType)
+enum class EBatteryPlayState : uint8
+{
+	EPlaying,
+	EGameOver,
+	EWon,
+	EUnknown
+};
+
 UCLASS(minimalapi)
 class ABatteryPickupGameMode : public AGameModeBase
 {
@@ -13,14 +23,34 @@ class ABatteryPickupGameMode : public AGameModeBase
 
 public:
 	ABatteryPickupGameMode();
-
+	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
 
 protected:
 	/** The rate at which the character loses power */
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Power", Meta = (BlueprintProtected = true))
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Battery Pickup", Meta = (BlueprintProtected = true))
 		float DecayRate;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Battery Pickup", Meta = (BlueprintProtected = true))
+		float PowerToWin;
+public:
+	UFUNCTION(BlueprintPure, category = "Battery Pickup")
+		float GetPowerToWin();
+
+
+protected:
+	/** The Widget class to use for our HUD screen */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Battery Pickup", Meta = (BlueprintProtected = true))
+		TSubclassOf<class UUserWidget> HUDWidgetClass;
+	/** The instance of the HUD */
+	UPROPERTY()
+		class UUserWidget* CurrentWidget;
+	private:
+	//set the current state
+	EBatteryPlayState CurrentState;
+
+public:
+	UFUNCTION(BlueprintPure, Category = "Battery Pickup")
+		EBatteryPlayState GetCurrentState() const;
+
+	void SetCurrentState(EBatteryPlayState state);
 };
-
-
-
